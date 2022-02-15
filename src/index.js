7ca097fe7dde4e0b7136bbcf8536c98b23f1de85
Reem <janinareem@gmail.com>
2022-02-15 17:37:20 +0200

    Add the basic file to render all component

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import TodoContainer from './components/ToDoContainer';
+import { BrowserRouter as Router } from "react-router-dom"
+import './App.css'
+
+ReactDOM.render(
+  <React.StrictMode>
+  <Router>
+    <TodoContainer />
+    </Router>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
