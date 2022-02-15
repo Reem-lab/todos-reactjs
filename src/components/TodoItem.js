16be15815cb16a40072be798f29ca9b893fdc42e
Reem <janinareem@gmail.com>
2022-02-15 17:31:59 +0200

    Add the task item

---
+++ src/components/TodoItem.js
@@ -0,0 +1,69 @@
+import React from "react";
+import styles from './TodoItem.module.css'
+
+class TodoItem extends React.Component{
+    constructor(props){
+        super(props)
+        this.state = {
+            editing: false,
+          }
+
+    }
+
+
+    completedStyle = {
+        fontStyle: "italic",
+        color: "#595959",
+        opacity: 0.4,
+        textDecoration: "line-through",
+      }
+
+     handleEditing = () => {
+        this.setState({
+            editing: true,
+          })
+        
+      }
+
+      handleUpdatedDone = event => {
+        if (event.key === "Enter") {
+            this.setState({ editing: false })
+          }
+      }
+      
+render(){
+    let viewMode = {}
+let editMode = {}
+
+if (this.state.editing) {
+  viewMode.display = "none"
+} else {
+  editMode.display = "none"
+}
+
+  return  (
+         <li className={styles.item}>
+        <div onDoubleClick={this.handleEditing} style={viewMode}>
+          <input 
+            type="checkbox" 
+            className={styles.checkbox}
+            checked={this.props.todo.completed}  
+            onChange={() => this.props.handleChangeProps(this.props.todo.id)}
+          />
+          <button onClick={() => this.props.deleteTodoProps(this.props.todo.id)}>Delete</button>
+          <span style={this.props.todocompleted ? this.completedStyle : null} >{this.props.todo.title}</span>
+        </div>
+        <input 
+          type="text" 
+          style={editMode} 
+          className={styles.textInput} 
+          value={this.props.todo.title} 
+          onChange={(e)=> {this.props.setUpdate(e.target.value, this.props.todo.id)}}
+          onKeyDown={this.handleUpdatedDone}
+        />
+      </li>
+  )
+}
+}
+
+export default TodoItem
