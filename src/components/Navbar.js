cd0f448809037a5c2ef7af6f3993c405e2e480a8
Reem <janinareem@gmail.com>
2022-02-15 17:29:22 +0200

    Add header and inputTodo and navbar component

---
+++ src/components/Navbar.js
@@ -0,0 +1,39 @@
+import React from "react"
+import { NavLink } from "react-router-dom"
+
+
+const Navbar = () => {
+    const links = [
+        {
+          id: 1,
+          path: "/",
+          text: "Home",
+        },
+        {
+          id: 2,
+          path: "/about",
+          text: "About",
+        },
+        {
+            id:3,
+            path: "/NotMatches",
+            text: "Not Match",
+        },
+      ]
+  return (
+    <nav className="navBar">
+    <ul>
+      {links.map(link => {
+        return (
+          <li key={link.id}>
+            <NavLink to={link.path} activeClassName="active-link" exact>
+                {link.text}
+                </NavLink>
+          </li>
+        )
+      })}
+    </ul>
+  </nav>
+  )
+}
+export default Navbar
